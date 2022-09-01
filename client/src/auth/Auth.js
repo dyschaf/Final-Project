fa6864b2adce4b4229fee239967221a2ce1bcfbf
dyschaf <schaffeldovid@gmail.com>
2022-09-01 19:03:24 +0300

    login working

---
+++ client/src/auth/Auth.js
@@ -0,0 +1,36 @@
+import {useEffect,useState,useContext} from 'react';
+import {useNavigate} from 'react-router-dom';
+import axios from 'axios';
+import {AppContext} from '../App';
+
+
+export const Auth = (props) =>{
+    const[redirect,setRedirect] = useState(null);
+    const {setAccessToken} = useContext(AppContext);
+    const navigate = useNavigate();
+
+    useEffect(()=>{
+      const verify = async()=>{
+        try{
+          let response = await axios.get('/token',{
+            withCredentials:true,
+            headers:{
+              'Content-Type':'application/json'
+            }
+          });
+          console.log(response);
+          if(response.status == 200){
+            setRedirect(true)
+          }
+        }
+        catch(e){
+          navigate('/login')
+        }
+      }
+      verify();
+    },[])
+
+    return (
+      redirect ? props.children : <h1>Unauthorized</h1>
+    )
+}
---
+++ controllers/Users.js
@@ -0,0 +1,70 @@
+import Users from "../models/UsersModel.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+export const getUsers = async (req, res) => {
+  try {
+    const users = await Users.findAll({
+      attributes: ["id", "email"],
+    });
+    res.json(users);
+  } catch (e) {
+    res.json({ msg: "not found" });
+    console.log(e);
+  }
+};
+
+export const SignUp = async (req, res) => {
+  const { email, password } = req.body;
+  const salt = await bcrypt.genSalt();
+  const hashPassword = await bcrypt.hash(password, salt);
+  try {
+    await Users.create({
+      email: email,
+      password: hashPassword,
+    });
+    res.json({ msg: "Register Successful" });
+  } catch (e) {
+    console.log(e);
+    res.status(404).json({ msg: `${e} Email already exist` });
+  }
+};
+
+export const Login = async (req, res) => {
+  try {
+    const user = await Users.findAll({
+      where: {
+        email: req.body.email,
+      },
+    });
+    const match = await bcrypt.compare(req.body.password, user[0].password);
+
+    if (!match) return res.status(400).json({ msg: "Wrong password" });
+
+    const userId = user[0].id;
+    const email = user[0].email;
+
+    const accessToken = jwt.sign(
+      { userId, email },
+      process.env.ACCESS_TOKEN_SECRET,
+      {
+        expiresIn: "3600s",
+      }
+    );
+    res.cookie("accessToken", accessToken, {
+      httpOnly: true,
+      maxAge: 60 * 60 * 1000,
+    });
+
+    res.json(accessToken);
+  } catch (e) {
+    res.status(404).json({ msg: `${e} Email not found` });
+  }
+};
+
+export const Logout = (req, res) => {
+  const accessToken = req.cookie.accessToken;
+  if (!accessToken) return res.sendStatus(204);
+  res.clearCookie("accessToken");
+  return res.sendStatus(200);
+};
