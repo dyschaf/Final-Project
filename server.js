432f0ec347b99e19cdb0f5ba4620708f1686fc3e
dyschaf <schaffeldovid@gmail.com>
2022-09-07 21:21:27 +0300

    for the build

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ import router from "./routes/Users.js";
 
 dotenv.config();
 const app = express();
-
+const __dirname = path.resolve()
 // app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(cookieParser());
 app.use(express.json());
