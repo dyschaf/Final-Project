bc3b4bc5115eff943e75b265563c9e847dcbb7eb
dyschaf <schaffeldovid@gmail.com>
2022-09-08 13:02:46 +0300

    new api

--- server.js
+++ server.js
@@ -10,7 +10,9 @@ dotenv.config();
 const app = express();
 const __dirname = path.resolve();
 // app.use(cors());
-app.use(cors({ credentials: true, origin: "https://herokuapp.com" }));
+app.use(
+  cors({ credentials: true, origin: "https://gsmarena-api.herokuapp.com/" })
+);
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
